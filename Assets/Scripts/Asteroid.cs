941e451a96e415555f73bfdabab1dc87daef55f4
Manmellon <24492231+Manmellon@users.noreply.github.com>
2022-03-06 13:46:54 +0300

    Added sprites arrays to asteroid

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Asteroid : MovingObject
 {
+    [SerializeField] Sprite[] _asteroidSprites;
+    [SerializeField] Sprite[] _asteroidFragmentSprites;
+
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    [SerializeField] Collider2D _collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int r = Random.Range(0, _asteroidSprites.Length);
+        _spriteRenderer.sprite = _asteroidSprites[r];
+
+        _collider = gameObject.AddComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
